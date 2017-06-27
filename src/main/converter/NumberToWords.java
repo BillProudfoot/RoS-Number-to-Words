9fdbe274d35c99fbb7de408cc0945e0ca2ed4dc6
Bill Proudfoot <user@CODECLAN031.local>
2017-06-27 19:58:17 +0100

    factored for invalid, input, ouput rather than number and words

--- src/main/converter/NumberToWords.java
+++ src/main/converter/NumberToWords.java
@@ -10,7 +10,40 @@ public class NumberToWords {
 
       string singleMap [] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 
-      string tensMap [] = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" }
+      string tensMap [] = { "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+      string NumberToWords(int input) {
+        if (input == 0)
+          return "invalid";
+
+        string output = "";
+
+        if ((input/1000000) > 0 ) {
+          output += NumberToWords(input/1000000) + " million ";
+          input %= 1000000;
+        }
+
+        if ((input/1000) > 0 ) {
+          output += NumberToWords(input/1000) + " thousand ";
+          input %= 1000;
+        }
+
+        if ((number/100) > 0 ) {
+          output += NumberToWords(number/100) + " hundred ";
+          input %=100;
+        }
+
+        if (input > 0) {
+          if (number < 20)
+            input += singleMap[input];
+          else {
+            output += tensMap[input];
+            if ((input %10) > 0)
+              output += singleMap[input % 10];
+          }
+        }
+        return output;
+      }
 
 
 
